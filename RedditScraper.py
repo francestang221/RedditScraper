b27d2853208c0402423cda0c9f4f6efcaa7cf956
Frances Tang <francest221@gmail.com>
2021-08-09 19:43:31 -0700

    Update RedditScraper.py

--- RedditScraper.py
+++ RedditScraper.py
@@ -23,13 +23,27 @@ def reddit_scraper(subreddit, topic):
     r = get_reddit(subreddit, topic)
     dict1 = r['data']['children']
     n = 1
-    result = {"text": ""}
+    all_posts = ""
     num_of_posts = len(dict1) if len(dict1) <= 10 else 10
+    # combine all 10 posts in one text string
     for i in range(num_of_posts):
         dict2 = dict1[i]
-        result['text'] += "# {}:".format(n) + dict2['data']['title']
-        result['text'] += "\n"
-        result['text'] += dict2['data']['selftext']
+        all_posts += "# {}:".format(n) + dict2['data']['title']
+        all_posts += "\n"
+        all_posts += dict2['data']['selftext']
         n += 1
+
+    # add each post separately
+    sep_posts = {}
+    for i in range(num_of_posts):
+        dict2 = dict1[i]
+        post_key = "post {}".format(i+1)
+        title = dict2['data']['title']
+        text = dict2['data']['selftext']
+        sep_posts[post_key] = {"title": title, "text": text}
+        n += 1
+
+    # combine both results together
+    result = {"text": all_posts, "each post": sep_posts}
     return json.dumps(result)
 
